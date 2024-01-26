2ea78ab3f1b82643124ffd8e4904ec505d2d3c1a
Divyansh Mehta <devthedeveloper1749@gmail.com>
2024-01-26 05:23:59 +0000

    Dynamic Array Init...

---
+++ Dynamic Arrays/src/App.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        /*
+         *                  Dynamic Arrays
+         * 
+         *  Advantages :
+         *  1. Random access of elements O(1)
+         *  2. Good locality of refrence and data cache utilization
+         *  3. Easy to insert/delete at the end
+         * 
+         *  Disadvantages :
+         *  1. Wastes more memory
+         *  2. Shifting elements is time consuming O(n)
+         *  3. Expanding/Shrinking the array is time consuming O(n)
+         * 
+         */
+
+         
+    }
+}
